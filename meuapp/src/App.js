0aa7b282d140d402a3a4cfbb0d141ab0297a9108
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-11-26 09:42:42 -0300

    first exercise

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,7 +1,57 @@
 import React from 'react'
 
-const App = () =>{
-  return <div>Infinity Quasar me dá um emprego!!</div>
-}
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  
+  const corAtivo = {
+    color:'green'
+  }
+  const corInativo ={
+    color:'red'
+  }
+
+  const dados = luana;
+
+  let total =0;
+  let valor =0;
+
+  dados.compras.forEach((compra) => {
+  valor = compra.preco.substring(3, compra.preco.length);
+  total += Number(valor)
+});
+
+
+  return (<div>
+    <p>Nome: {dados.cliente}</p>
+    <p>Idade {dados.idade}</p>
+    <span>Situação: </span>
+    <span style = {dados.ativa ? corAtivo : corInativo} > {dados.ativa ? 'Ativa'  : 'Inativa'}</span>
+    <p>Total gasto: R$ {total}</p>
+    {total > 10000 ? <p> Você está gastando demais!!</p> : ''}
+  </div>);
+
+};
 
 export default App;
