3b1b159bd36a229d84de0d195d39fcc3bc4267b4
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-06 11:46:34 -0300

    state exercises

--- meuapp/src/ButtonModal.js
+++ meuapp/src/ButtonModal.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const ButtonModal = ({setModal}) => {
+
+    function handleClick(){
+        setModal((ativo) => !ativo)
+    }
     
 
     return (
-        <button onClick={() => setModal(true)}>
+        <button onClick={handleClick}>
             Abrir
         </button>
     )
