a4715b5d79df92708c10ba49d028e0dcf7e7f0e6
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-06 12:03:42 -0300

    more state counter exercises

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -5,16 +5,21 @@ import React from "react";
 const App = () => {
   
 const [contador , setContador] = React.useState(0)
+const [items, setItems] = React.useState(['número 0'])
 
 function handleClick(){
   setContador((contador)=> {
     return contador + 1
   })
+
+  setItems((items) => [...items, 'número ' + (contador + 1)]
+  )
 }
 
   return(
     <>
-    <h1>Contador:</h1>
+    <h1>Contador: {items.map((item) => 
+    <li>{item}</li>)}</h1>
     <button onClick = {handleClick}>{contador}</button>
     </>
   )};
