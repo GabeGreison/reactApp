b7d11cf592793e62a3b9ad0adc176161503e8169
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-14 22:27:52 -0300

    useContext exercises

--- meuapp/src/Produto.js
+++ meuapp/src/Produto.js
@@ -3,7 +3,16 @@ import { GlobalContext } from "./GlobalContext";
 
 const Produto = () => {
   const global = React.useContext(GlobalContext);
-  return <div>Produto: {global.nome}</div>;
+  if (global.dados === null) return null;
+  return (
+    <div>
+      {" "}
+      Produto:{" "}
+      {global.dados.map((produto) => (
+        <li key={produto.id}>{produto.nome}</li>
+      ))}{" "}
+    </div>
+  );
 };
 
 export default Produto;
