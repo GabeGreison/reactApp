e8b1f4217633a44d82e1f4a4bd511106fe7362e8
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-17 22:48:42 -0300

    React Forms

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,40 +1,17 @@
 import React from "react";
-import useFetch from "./useFetch";
-import useLocalStorage from "./useLocalStorage";
 
 const App = () => {
-  const [produto, setProduto] = useLocalStorage("produto", "");
-  const { request, data, loading, error } = useFetch();
-
-  React.useEffect(() => {
-    async function fetchData() {
-      const { response, json } = request(
-        "https://ranekapi.origamid.dev/json/api/produto/"
-      );
-    }
-    fetchData();
-  }, [request]);
-
-  function handleClick({ target }) {
-    setProduto(target.innerText);
+  const [nome, setNome] = React.useState("");
+  function handleChange(event) {
+    setNome(event.target.value);
   }
-  if (error) return <p>{error}</p>;
-  if (loading) return <p>Carregando...</p>;
-  if (data)
-    return (
-      <div>
-        <p>Produto Preferido : {produto}</p>
-        <button onClick={handleClick}>notebook</button>
-        <button onClick={handleClick}>smartphone</button>
 
-        {data.map((produto) => (
-          <div key={produto.id}>
-            <h1>{produto.nome}</h1>
-          </div>
-        ))}
-      </div>
-    );
-  else return null;
+  return (
+    <form>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" value={nome} onChange={handleChange} id="nome" />
+    </form>
+  );
 };
 
 export default App;
