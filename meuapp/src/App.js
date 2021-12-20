bc7fe766f3115da2d7eceff90e05ed1a37c10134
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-20 16:01:13 -0300

    textarea exercise

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,95 +1,22 @@
 import React from "react";
 
-const formFields = [
-  {
-    id: "nome",
-    label: "Nome",
-    type: "text",
-  },
-  {
-    id: "email",
-    label: "Email",
-    type: "email",
-  },
-  {
-    id: "senha",
-    label: "Senha",
-    type: "password",
-  },
-  {
-    id: "cep",
-    label: "Cep",
-    type: "text",
-  },
-  {
-    id: "rua",
-    label: "Rua",
-    type: "text",
-  },
-  {
-    id: "numero",
-    label: "Numero",
-    type: "text",
-  },
-  {
-    id: "bairro",
-    label: "Bairro",
-    type: "text",
-  },
-  {
-    id: "cidade",
-    label: "Cidade",
-    type: "text",
-  },
-  {
-    id: "estado",
-    label: "Estado",
-    type: "text",
-  },
-];
-
 const App = () => {
-  const [form, setForm] = React.useState({
-    nome: "",
-    email: "",
-    senha: "",
-    cep: "",
-    rua: "",
-    numero: "",
-    bairro: "",
-    cidade: "",
-    estado: "",
-  });
-  const [response, setResponse] = React.useState(null);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    fetch("https://ranekapi.origamid.dev/json/api/usuario", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(form),
-    }).then((response) => {
-      setResponse(response);
-    });
-  }
+  const [texto, setTexto] = React.useState("");
 
   function handleChange({ target }) {
-    const { id, value } = target;
-    setForm({ ...form, [id]: value });
+    setTexto(target.value);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      {formFields.map(({ id, label, type }) => (
-        <div key={id}>
-          <label htmlFor={id}>{label}</label>
-          <input type={type} id={id} value={form[id]} onChange={handleChange} />
-        </div>
-      ))}
-      {response && response.ok && <p>Usuário criado</p>}
-      <button>Enviar</button>
+    <form>
+      <textarea
+        name="texto"
+        id="texto"
+        rows="5"
+        value={texto}
+        onChange={handleChange}
+      />
+      <button>enviar</button>
     </form>
   );
 };
