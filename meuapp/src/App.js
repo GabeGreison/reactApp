604f6a1c06be7ff2ac5e4beb96d8f680d66c348b
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-21 22:05:40 -0300

    Radio Components

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,28 +1,30 @@
 import React from "react";
 import Input from "./Form/Input";
+import Radio from "./Form/Radio";
 import { Select } from "./Form/Select";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [produto, setProduto] = React.useState("");
+  const [cor, setCor] = React.useState("");
+  const [fruta, setFruta] = React.useState("");
   return (
     <form>
+      <Radio options={["laranja", "uva"]} value={fruta} setValue={setFruta} />
       <Select
         options={["Smartphone", "Notebook", "Desktop"]}
         value={produto}
         setValue={setProduto}
         required
-        nome="produtos"
       />
       <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
       <button>Enviar</button>
       <Select
         options={["verde", "azul", "vermelho"]}
-        value={produto}
-        setValue={setProduto}
-        nome="cor"
+        value={cor}
+        setValue={setCor}
       />
     </form>
   );
