336b91e04b40d3708fc08d264944003f89bcbe62
Gabriel Greison <gabrielgsousa72@gmail.com>
2021-12-27 15:50:40 -0300

    custom form hook

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,35 +1,14 @@
 import React from "react";
+import useForm from "./Hooks/useForm";
 import Input from "./Form/Input";
 
 const App = () => {
-  const [cep, setCep] = React.useState("");
-  const [error, setError] = React.useState(null);
+  const cep = useForm("cep");
+  const email = useForm("email");
 
-  function validateCep(value) {
-    if (value.length === 0) {
-      setError("Preencha um valor");
-      return false;
-    } else if (!/^\d{5}-?\d{3}$/.test(value)) {
-      setError("Preencha um CEP valido ");
-      return false;
-    } else {
-      setError(null);
-      return true;
-    }
-  }
-
-  function handleBlur({ target }) {
-    validateCep(target.value);
-  }
-
-  function handleChange({ target }) {
-    if (error) validateCep(target.value);
-
-    setCep(target.value);
-  }
   function handleSubmit(event) {
     event.preventDefault();
-    if (validateCep(cep)) {
+    if (cep.validate() && email.validate()) {
       console.log("Enviou");
     } else {
       console.log("Não enviou");
@@ -42,11 +21,15 @@ const App = () => {
         type="text"
         id="cep"
         placeholder="000000-000"
-        value={cep}
-        onChange={handleChange}
-        onBlur={handleBlur}
+        {...cep}
       />
-      {error && <p>{error}</p>}
+      <Input
+        label="Email"
+        type="text"
+        id="email"
+        placeholder="gabriel@gmail.com"
+        {...email}
+      />
       <button>Enviar</button>
     </form>
   );
